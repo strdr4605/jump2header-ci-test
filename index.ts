1770a56fcc817cb6cbb8e764a0c2ac5c97ec9f07
Dragoș Străinu <str.dr4605@gmail.com>
2020-12-12 22:35:48 +0200

    feat: fork repo from issue description

--- index.ts
+++ index.ts
@@ -1,4 +1,34 @@
-import { context } from "@actions/github";
-// import { PayloadRepository } from "@actions/github/lib/interfaces";
+import { context, getOctokit } from "@actions/github";
+import { WebhookPayload } from "@actions/github/lib/interfaces";
+
+const octokit = getOctokit(process.env.GITHUB_TOKEN || "");
+
+const { issue }: WebhookPayload = context.payload;
 
 console.log(JSON.stringify(context.payload));
+
+const FORK_REPO_REGEX = /Fork repo: (?<owner>.+)\/(?<repo>.+)/;
+
+const regExpExecArray: RegExpExecArray =
+  FORK_REPO_REGEX.exec(issue?.body || "") ||
+  ({ groups: {} } as RegExpExecArray);
+
+const { owner, repo } = regExpExecArray.groups || { owner: "", repo: "" };
+
+fork(owner, repo);
+
+async function fork(owner: string, repo: string) {
+  if (!owner || !repo) {
+    console.log("No owner or repo", owner, repo);
+    return;
+  }
+
+  try {
+    await octokit.request("POST /repos/{owner}/{repo}/forks", {
+      owner,
+      repo,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
